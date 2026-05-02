e4c1792290d9a9977f5e8855be7a9d6098a642e0
Yazrol Surya <yazrol.surya60@gmail.com>
2026-05-02 12:59:51 +0700

    Penambahan method getConn

--- config/Database.php
+++ config/Database.php
@@ -1,17 +1,25 @@
 <?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db_name = "db_sistem_pos";
+class Database
+{
 
-// Membuat koneksi MySQLi secara global
-$conn = new mysqli($host, $user, $pass, $db_name);
+    private $host = "localhost";
+    private $user = "root";
+    private $pass = "";
+    private $db_name = "db_sistem_pos";
+    protected $conn;
 
-// Cek koneksi
-if ($conn->connect_error) {
-    die("Koneksi gagal: " . $conn->connect_error);
+    public function __construct()
+    {
+        if (!isset($this->conn)) {
+            $this->conn = new mysqli($this->host, $this->user, $this->pass, $this->db_name);
+            if ($this->conn->connect_error) {
+                die("Koneksi gagal: " . $this->conn->connect_error);
+            }
+        }
+    }
+    public function getConn()
+    {
+        return $this->conn;
+    }
 }
-
-// Set agar mysqli menampilkan exception jika ada error (mirip PDOException)
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 ?>
